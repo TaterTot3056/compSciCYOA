aab55663663d0711bf2c2142d2e01833969918d3
Taylor  Quinn <taylorquinn@taylors-macbook-air.local>
2017-10-04 14:26:49 -0400

    Initial Commit

---
+++ chooseYourOwnAdventure/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  chooseYourOwnAdventure
+//
+//  Created by Taylor  Quinn on 10/4/17.
+//  Copyright © 2017 Taylor  Quinn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
